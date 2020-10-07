41a1740f5ceb57cc04d216a211ffd9f3b04d9fe5
celia <celia_hassaine@hotmail.com>
2020-10-07 18:35:29 +0200

    problème résolu

--- Backend/Pot_App/settings.py
+++ Backend/Pot_App/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appli',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
---
+++ Backend/appli/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppliConfig(AppConfig):
+    name = 'appli'
