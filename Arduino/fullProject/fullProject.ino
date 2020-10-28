5cd73fcb7f721be7852cc00d8da76981c8c29bf3
Florent Theys <f.theys@students.ephec.be>
2020-10-28 22:36:44 +0100

    sonde de températures et d'humidité

--- Arduino/fullProject/fullProject.ino
+++ Arduino/fullProject/fullProject.ino
@@ -1,8 +1,13 @@
+#include <DHT.h>
+
 #define NOT_AN_INTERRUPT -1
 
 #define flowsensorpin 2  //Defininig a pin
+#define DHTPIN 7
 #define AnalogPin0 A0 
 #define AnalogPin1 A1
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 
 
@@ -10,6 +15,10 @@ unsigned long currentTime;
 volatile int flow_frequency;
 float vol = 0.0,l_minute;
 int amountparcels = 2;
+float sensorValue = 0;
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
 
 
 
@@ -19,6 +28,7 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(flowsensorpin), flow, RISING); // Setup Interrupt
     reset();
     currentTime = millis();
+    dht.begin();
 }
 
 void reset(){
@@ -33,12 +43,31 @@ void flow (){
 void loop() {
 //Probes and data collection
 
- 
+   for (int i = 0; i <= 100; i++) 
+  { 
+    sensorValue = sensorValue + analogRead(AnalogPin0); 
+    delay(1); 
+  } 
+  sensorValue = sensorValue/100.0; 
+  Serial.println(sensorValue); 
+  delay(30); 
+
+    delay(2000);
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    //delay(10000); //Delay 2 sec.
+
 
 //Watering and amount
 
    if(flow_frequency != 0){
-
       l_minute = (flow_frequency / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
       l_minute = l_minute/60;
       vol = vol +l_minute;
