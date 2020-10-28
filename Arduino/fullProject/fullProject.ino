3cc6a48caac327b6b4ac06e6afdef2f79860ecb7
Florent Theys <f.theys@students.ephec.be>
2020-10-28 23:13:10 +0100

    ouverture/fermeture valve

--- Arduino/fullProject/fullProject.ino
+++ Arduino/fullProject/fullProject.ino
@@ -1,16 +1,14 @@
 #include <DHT.h>
 
-#define NOT_AN_INTERRUPT -1
-
-#define flowsensorpin 2  //Defininig a pin
+#define FLOWSENSORPIN 2  //Defininig a pin
 #define DHTPIN 7
-#define AnalogPin0 A0 
-#define AnalogPin1 A1
+#define ANALOGPIN0 A0
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define SOLENOIDPIN 4
+
+
+
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-
-
-
 unsigned long currentTime;
 volatile int flow_frequency;
 float vol = 0.0,l_minute;
@@ -23,34 +21,34 @@ float temp; //Stores temperature value
 
 
 void setup() {
-    pinMode(flowsensorpin, INPUT);
-    Serial.begin(9600);
-    attachInterrupt(digitalPinToInterrupt(flowsensorpin), flow, RISING); // Setup Interrupt
+  pinMode(FLOWSENSORPIN, INPUT);
+  pinMode(SOLENOIDPIN, OUTPUT);
+  Serial.begin(9600);
+    attachInterrupt(digitalPinToInterrupt(FLOWSENSORPIN), flow, RISING); // Setup Interrupt
     reset();
     currentTime = millis();
     dht.begin();
-}
+  }
 
-void reset(){
-  int soilsensordata[amountparcels];
-}
+  void reset(){
+    int soilsensordata[amountparcels];
+  }
 
-void flow (){
+  void flow (){
+    flow_frequency++;
+  }
 
-  flow_frequency++;
-  
-}
-void loop() {
-//Probes and data collection
+  void loop() {
 
-   for (int i = 0; i <= 100; i++) 
-  { 
-    sensorValue = sensorValue + analogRead(AnalogPin0); 
-    delay(1); 
-  } 
-  sensorValue = sensorValue/100.0; 
-  Serial.println(sensorValue); 
-  delay(30); 
+    //Probes and data collection
+
+    for (int i = 0; i <= 100; i++){
+      sensorValue = sensorValue + analogRead(ANALOGPIN0);
+      delay(1);
+    }
+    sensorValue = sensorValue/100.0;
+    Serial.println(sensorValue);
+    delay(30);
 
     delay(2000);
     //Read data and store it to variables hum and temp
@@ -65,19 +63,26 @@ void loop() {
     //delay(10000); //Delay 2 sec.
 
 
-//Watering and amount
+    //Watering and amount
 
-   if(flow_frequency != 0){
+    if(flow_frequency != 0){
       l_minute = (flow_frequency / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
       l_minute = l_minute/60;
       vol = vol +l_minute;
       Serial.print("Vol:");
-      Serial.print(vol);      
+      Serial.print(vol);
       Serial.print(" L ");
       flow_frequency = 0; // Reset Counter
       Serial.print(l_minute, DEC); // Print litres/hour
       Serial.println(" L/Sec ");
       delay(1000);
-     }
+    }
+
+    // Opening and closing valve
+
+    digitalWrite(SOLENOIDPIN, HIGH);
+    delay(1000);
+    digitalWrite(SOLENOIDPIN, LOW);
+    delay(1000);
   }
-   
+
