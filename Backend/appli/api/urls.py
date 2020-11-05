f615272e7870501b291f53c6a9ff2088ff2e797c
Glenn <mr.glennichou@gmail.com>
2020-11-05 14:46:23 +0100

    Swagger finitions

--- Backend/appli/api/urls.py
+++ Backend/appli/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from rest_framework import permissions
+from rest_framework_simplejwt import views as jwt_views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import routers
@@ -12,14 +13,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
-router = routers.DefaultRouter()
-router.register(r'plante', views.PlantesAPIView, basename="plantes")
-router.register(r'users', views.UserAPIView, basename="users")
-router.register(r'parcelle', views.ParcelleAPIView, basename="parcelles")
-router.register(r'parcelle-user', views.UserDetail, basename="parcellesUsers")
-router.register(r'donnees-parcelle', views.DonneesParcelleAPIView, basename="dParcelles")
-router.register(r'donnees-user', views.DonneesUserAPIView, basename="dUsers")
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -34,19 +27,23 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+router = routers.DefaultRouter()
+router.register(r'plante', views.PlantesAPIView, basename="plantes")
+router.register(r'users', views.UserAPIView, basename="users")
+router.register(r'parcelle', views.ParcelleAPIView, basename="parcelles")
+router.register(r'parcelle-user', views.UserDetail, basename="parcellesUsers")
+router.register(r'donnees-parcelle', views.DonneesParcelleAPIView, basename="dParcelles")
+router.register(r'donnees-user', views.DonneesUserAPIView, basename="dUsers")
+
+
 urlpatterns = [
-    url(r'^$', PlantesAPIView.as_view(), name='post-listcreate-plant'),
-    url(r'^(?P<pk>\d+)/$', PlantesRudView.as_view(), name='post-rud-plant'),
-    url(r'^users/$', UserAPIView.as_view(), name='post-listcreate-util'),
-    url(r'^categorie/(?P<pk>\d+)/$', UserRudView.as_view(), name='post-rud-util'),
-    url(r'^parcelle/$', ParcelleAPIView.as_view(), name='post-listcreate-parce'),
-    url(r'^parcelle/(?P<pk>\d+)/$', ParcelleRudView.as_view(), name='post-rud-parce'),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register', UserRegisterView.as_view()),
+    
+    path('token', jwt_views.TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    ##path('register', UserRegisterView.as_view()),
     path('', include(router.urls)),
 ]
     
