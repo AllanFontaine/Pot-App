b602f8c95646aa1e78500f1303ce93f8a11ece06
Glenn <mr.glennichou@gmail.com>
2020-11-17 17:48:14 +0100

    Lavage de notre template

--- Frontend/Pot-App/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/Pot-App/src/app/components/sidebar/sidebar.component.ts
@@ -12,13 +12,6 @@ export const ROUTES: RouteInfo[] = [
     { path: '/wiki', title: 'Wiki', icon: 'chrome_reader_mode', class: ''},
     { path: '/user-profile', title: 'User Profile',  icon: 'person', class: '' },
     { path: '/historique', title: 'Historique',  icon: 'archive', class: '' },
-    { path: '/table-list', title: 'Table List',  icon: 'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon: 'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon: 'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon: 'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon: 'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon: 'unarchive', class: 'active-pro' },
-
   ];
 
 @Component({
