b1549fa17119186a047b5dc466c2cdbdcf40af86
Florent Theys <f.theys@students.ephec.be>
2020-11-22 16:48:17 +0100

    lecture/écriture carte SD + mise en place clock

--- Arduino/fullProject/fullProject.ino
+++ Arduino/fullProject/fullProject.ino
@@ -1,14 +1,25 @@
 #include <DHT.h>
+#include "SD.h"
+#include "RTClib.h"
+#include <Wire.h>
 
-#define FLOWSENSORPIN 2  //Defininig a pin
-#define DHTPIN 7
-#define ANALOGPIN0 A0
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-#define SOLENOIDPIN 4
+#define LOG_INTERVAL  1000
+#define ECHO_TO_SERIAL   1
+#define WAIT_TO_START    0
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+
+//Defining PINS
+const int FlowSensorPin 2;
+const int DhtPin 7;
+const int SolenoidPin = 28;
+const int chipSelect = 10; // sd card
 
 
 
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+File log;
+RTC_DS1307 rtc;
+DateTime now;
+DHT dht(DhtPin, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 unsigned long currentTime;
 volatile int flow_frequency;
 float vol = 0.0,l_minute;
@@ -18,16 +29,52 @@ int chk;
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
 
-
+void error(char *str) {
+  Serial.print("error: ");
+  Serial.println(str);
+}
 
 void setup() {
-  pinMode(FLOWSENSORPIN, INPUT);
-  pinMode(SOLENOIDPIN, OUTPUT);
+  pinMode(FlowSensorPin, INPUT);
+  pinMode(SolenoidPin, OUTPUT);
   Serial.begin(9600);
-    attachInterrupt(digitalPinToInterrupt(FLOWSENSORPIN), flow, RISING); // Setup Interrupt
+    attachInterrupt(digitalPinToInterrupt(FlowSensorPin), flow, RISING); // Setup Interrupt
     reset();
     currentTime = millis();
     dht.begin();
+
+  // Cloack
+    Wire.begin();
+    if(!rtc.begin()) {
+      log.println("RTC failed");
+    #if ECHO_TO_SERIAL
+      Serial.println("RTC failed");
+    #endif  //ECHO_TO_SERIAL
+    }
+
+    // Set the time and date on the real time clock if necessary
+  if (! rtc.isrunning()) {
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+
+  //SD card
+  if(!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+
+  Serial.println("Card initialized.");
+
+  // create a new file
+  char filename[] = "test.CSV";
+  for(uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if(! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE);
+      break;  // leave the loop!
+    }
   }
 
   void reset(){
@@ -42,7 +89,7 @@ void setup() {
 
     //Probes and data collection
 
-    for (int i = 0; i <= 100; i++){
+    for(int i = 0; i <= 100; i++){
       sensorValue = sensorValue + analogRead(ANALOGPIN0);
       delay(1);
     }
@@ -80,9 +127,7 @@ void setup() {
 
     // Opening and closing valve
 
-    digitalWrite(SOLENOIDPIN, HIGH);
+    digitalWrite(SolenoidPin, HIGH);
     delay(1000);
-    digitalWrite(SOLENOIDPIN, LOW);
-    delay(1000);
-  }
 
+  }
