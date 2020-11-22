affc98b08a9d6488f8ffa7019d7fd0ef42454411
Florent Theys <f.theys@students.ephec.be>
2020-11-22 23:31:18 +0100

    délai de 30min entre chaque prise de donnée venant des sondes

--- Arduino/MasterFinal/MasterFinal.ino
+++ Arduino/MasterFinal/MasterFinal.ino
@@ -4,7 +4,6 @@
 
 #define DHTTYPE DHT22 // temp/moisture sensor
 #define ECHO_TO_SERIAL   1
-#define LOG_INTERVAL  1000
 
 //Defining PINS
 const int dhtPin = 7;
@@ -13,10 +12,12 @@ const int soilMoisturePin = A0;
 DHT dht(dhtPin, DHTTYPE);
 RTC_DS1307 rtc;
 DateTime now;
-unsigned long currentTime; // Since when the arduino start
 float soilMoisture = 0;
 float hum = 0;
 float temp = 0;
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long period = 4000; // put 1080000 for 30min
 
 void error(char *str) {
   Serial.print("error: ");
@@ -25,9 +26,9 @@ void error(char *str) {
 
 void setup() {
   Serial.begin(9600);
-  currentTime = millis();
   dht.begin();
-
+  startMillis = millis();
+  
   // Cloack
   Wire.begin();
   if(!rtc.begin()) {
@@ -42,27 +43,34 @@ void setup() {
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+
 }
 
 void loop() {
-  // Soil moiture sensor 
-  for(int i = 0; i <= 100; i++){
-    soilMoisture = soilMoisture + analogRead(soilMoisturePin);
-    delay(1);
-  }
-  soilMoisture = soilMoisture/100.0;
-  Serial.println(soilMoisture);
-
-  //Temperature and air humidity sensor
-  //Read data and store it to variables hum and temp
-  hum = dht.readHumidity();
-  temp= dht.readTemperature();
-  //Print temp and humidity values to serial monitor
-  Serial.print("Humidity: ");
-  Serial.print(hum);
-  Serial.print(" %, Temp: ");
-  Serial.print(temp);
-  Serial.println(" Celsius");
   
-  delay(2500);
+  currentMillis = millis();
+  if (currentMillis - startMillis >= period) {
+    // Soil moiture sensor 
+    for(int i = 0; i <= 100; i++){
+      soilMoisture = soilMoisture + analogRead(soilMoisturePin);
+      delay(1);
+    }
+    soilMoisture = soilMoisture/100.0;
+    Serial.println(soilMoisture);
+  
+    //Temperature and air humidity sensor
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+
+    //reset currentMillis
+    startMillis = millis();
+    currentMillis = millis();
+  }
 }
