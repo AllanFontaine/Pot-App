d109022be019b7a2f5ff94950f2abe96d868cc78
Glenn <mr.glennichou@gmail.com>
2020-12-08 18:02:47 +0100

    bugg

--- Frontend/Pot-App/src/app/service/wiki.service.ts
+++ Frontend/Pot-App/src/app/service/wiki.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class WikiService{
+export class WikiService {
     plants: [];
-    selectedPlant; 
+    selectedPlant;
     selectedPlantWikipedia;
     offset = 0;
     limit = 10;
@@ -13,27 +13,27 @@ export class WikiService{
     length;
     pageIndex = 0;
 
-    constructor(private http : HttpClient) {
+    constructor(private http: HttpClient) {
 
     }
 
-    get_plants():Observable<any> {
+    get_plants(): Observable<any> {
         return this.http.get('http://localhost:8000/api/plante/');
     }
 
-    get_plant(id: number):Observable<any> {
+    get_plant(id: number): Observable<any> {
         return this.http.get('http://localhost:8000/api/plante/' + id + '/');
     }
 
-    get_plant_by_name(nom: string, order: string, tri: string):Observable<any> {
+    get_plant_by_name(nom: string, order: string, tri: string): Observable<any> {
         return this.http.get("http://localhost:8000/api/plante/?name=" + nom + '&order=' + order + "tri=" + tri);
     }
 
-    get_plant_count():Observable<any> {
+    get_plant_count(): Observable<any> {
         return this.http.get("http://localhost:8000/api/plante/?count")
     }
 
-    get_plant_offset_limit(offset: number, limit: number, order: string, tri: string):Observable<any> {
+    get_plant_offset_limit(offset: number, limit: number, order: string, tri: string): Observable<any> {
         return this.http.get("http://localhost:8000/api/plante/?limit=" + limit + "&offset=" + offset + "&order=" + order + "&tri=" + tri);
     }
 }
