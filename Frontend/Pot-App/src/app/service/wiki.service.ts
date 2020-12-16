2c8264a0cdcaeb64c2e6bd768e1d5f4848a71156
Glenn <mr.glennichou@gmail.com>
2020-12-16 23:55:23 +0100

    sss

--- Frontend/Pot-App/src/app/service/wiki.service.ts
+++ Frontend/Pot-App/src/app/service/wiki.service.ts
@@ -12,7 +12,7 @@ export class WikiService {
     filter = "";
     length;
     pageIndex = 0;
-    urlApi = 'https://api.pot-app.be/api/'
+    urlApi = 'http://127.0.0.1:8000/api/'
     constructor(private http: HttpClient) {
 
     }
